f0021aa1a05c30ce0dc5d2e59878e830ca55b507
tantrieuf31@gmail.com <tantrieuf31@gmail.com@78ffa5e9-dbab-6ad7-afc5-a47219708ec2>
2010-10-22 16:17:14 +0000

    improve

--- php_server_app/PlaceMarker-Server/src/save_loc.php
+++ php_server_app/PlaceMarker-Server/src/save_loc.php
@@ -1,8 +1,13 @@
 <?php
-$myFile = "loc_data.txt";
-$fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = $_GET['loc'];
-$callback= $_GET['callback'];
-fwrite($fh, $stringData."\n");
-fclose($fh);
-echo $callback.'("'.$stringData.'");';
+if( isset($_GET['loc']) && isset($_GET['callback']) ) {
+	$myFile = "loc_data.txt";
+	$fh = fopen($myFile, 'a') or die("can't open file");
+	$today = date("F j, Y, g:i a");
+	$stringData = $_GET['loc']."@".$today."\n";
+	$callback= $_GET['callback'];
+	fwrite($fh, $stringData);
+	fclose($fh);
+	echo $callback.'("'.$stringData.'");';
+} else {
+	echo 'alert("Error: param loc and callback should be set! ");';
+}
