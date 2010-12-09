7f509d48d53aa813ecf78cd62cb7eaecf4891335
tantrieuf31@gmail.com <tantrieuf31@gmail.com@78ffa5e9-dbab-6ad7-afc5-a47219708ec2>
2010-12-09 07:31:18 +0000

    update and improve GUI

--- OptimizerJS/src/optimizerjs/IOHelper.java
+++ OptimizerJS/src/optimizerjs/IOHelper.java
@@ -3,6 +3,7 @@ package optimizerjs;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,6 +12,10 @@ import java.io.Reader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -50,5 +55,67 @@ public class IOHelper {
         }
         int size = (int) Math.ceil((content.getBytes().length / 1024));
         return size;
+    }
+    
+    public static String getParentDirPath(){
+        String currentdir = System.getProperty("user.dir");        
+        try {
+            File dir2 = new File("..");            
+            return dir2.getCanonicalPath();
+        } catch (IOException ex) {
+            Logger.getLogger(OptimizerJS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //FIXME remove hardcode
+        return currentdir.replace("/OptimizerJS", "");
+    }
+
+    /**
+     * Recursively walk a directory tree and return a List of all
+     * Files found; the List is sorted using File.compareTo().
+     *
+     * @param aStartingDir is a valid directory, which can be read.
+     */
+    public static List<File> getFileListing(File aStartingDir) throws FileNotFoundException {
+        validateDirectory(aStartingDir);
+        List<File> result = getFileListingNoSort(aStartingDir);
+ //       Collections.sort(result);
+        return result;
+    }
+
+    // PRIVATE //
+    private static List<File> getFileListingNoSort(
+            File aStartingDir) throws FileNotFoundException {
+        List<File> result = new ArrayList<File>();
+        File[] filesAndDirs = aStartingDir.listFiles();
+        List<File> filesDirs = Arrays.asList(filesAndDirs);
+        for (File file : filesDirs) {
+            result.add(file); //always add, even if directory
+            if (!file.isFile()) {
+                //must be a directory
+                //recursive call!
+                List<File> deeperList = getFileListingNoSort(file);
+                result.addAll(deeperList);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Directory is valid if it exists, does not represent a file, and can be read.
+     */
+    private static void validateDirectory(
+            File aDirectory) throws FileNotFoundException {
+        if (aDirectory == null) {
+            throw new IllegalArgumentException("Directory should not be null.");
+        }
+        if (!aDirectory.exists()) {
+            throw new FileNotFoundException("Directory does not exist: " + aDirectory);
+        }
+        if (!aDirectory.isDirectory()) {
+            throw new IllegalArgumentException("Is not a directory: " + aDirectory);
+        }
+        if (!aDirectory.canRead()) {
+            throw new IllegalArgumentException("Directory cannot be read: " + aDirectory);
+        }
     }
 }
