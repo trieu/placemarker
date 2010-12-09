19937c23f09fad958d3d7a9fd107ce98adc7c9a5
tantrieuf31@gmail.com <tantrieuf31@gmail.com@78ffa5e9-dbab-6ad7-afc5-a47219708ec2>
2010-12-09 08:43:09 +0000

    update and improve

--- trunk/OptimizerJS/src/optimizerjs/OptimizerJS.java
+++ trunk/OptimizerJS/src/optimizerjs/OptimizerJS.java
@@ -42,6 +42,8 @@ public class OptimizerJS {
         
         String scriptTag = "<script type='text/javascript' charset='utf-8' src='optimized-all.js'></script>\n </head>";
         htmlSrc = htmlSrc.replaceAll("</head>",scriptTag);//put at the bottom head       
+        htmlSrc = htmlSrc.replaceAll("\\n", "");
+        htmlSrc = htmlSrc.replaceAll("\\t", "");
         
         IOHelper.writeStringToFile(htmlSrc, optimizedHtmlSrcPath);        
         return normalJSPaths;
