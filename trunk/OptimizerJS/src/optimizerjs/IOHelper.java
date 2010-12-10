052df37162b33836dc21fc2ab4505937f0f5970b
tantrieuf31@gmail.com <tantrieuf31@gmail.com@78ffa5e9-dbab-6ad7-afc5-a47219708ec2>
2010-12-10 16:51:47 +0000

    update

--- trunk/OptimizerJS/src/optimizerjs/IOHelper.java
+++ trunk/OptimizerJS/src/optimizerjs/IOHelper.java
@@ -14,22 +14,20 @@ import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author tantrieuf31@gmail.com (Trieu Nguyen)
  */
 public class IOHelper {
 
-    public static String loadStringFromFile(String htmlFilePath) {
+    public static String loadStringFromFile(String filePath) {
         try {
             Writer writer = new StringWriter();
             char[] buffer = new char[1024];
-            Reader reader = new InputStreamReader(new FileInputStream(htmlFilePath), "UTF-8");
+            Reader reader = new InputStreamReader(new FileInputStream(filePath), "UTF-8");
             int n;
             while ((n = reader.read(buffer)) != -1) {
                 writer.write(buffer, 0, n);
@@ -43,9 +41,14 @@ public class IOHelper {
     }
 
     public static int writeStringToFile(String content, String toFilePath) {
-        File compiledFile = new File(toFilePath);
         try {
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(compiledFile), "UTF-8"));
+            File theFile = new File(toFilePath);
+            if( ! theFile.exists() ){
+                if( ! theFile.createNewFile() ) {
+                    return 0;
+                }
+            }
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(theFile), "UTF-8"));
             out.write(content);
             out.close();
         } catch (UnsupportedEncodingException ue) {
